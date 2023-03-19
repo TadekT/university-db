ddd25735d6d93c1b688756e8c34e6bfe49baf155
Tadek <tadeusz.toporkiewicz@gmail.com>
2023-03-19 22:52:09 +0100

    Reorganizeit code

--- Student.cpp
+++ Student.cpp
@@ -3,32 +3,38 @@
 #include <algorithm>
 #include <iostream>
 
-//constructors
+// constructors
 Student::Student()
-:indexNumber_(-1)
-{std::cout<<"Error , need more data\n\n";};
+    : indexNumber_(-1)
+{
+    std::cout << "Error , need more data\n\n";
+};
 
 Student::Student(
-    std::string n
-    ,std::string sn
-    ,std::string a
-    ,int in
-    ,int p
-    ,std::string g)
-    :name_(n),surname_(sn),addres_(a),indexNumber_(in),pesel_(p),gender_(g)
-    {};
+    std::string n, std::string sn, std::string a, int in, int p, std::string g)
+    : name_(n), surname_(sn), addres_(a), indexNumber_(in), personalID_(p), gender_(g){};
 
-//setters
-    void Student::setName(std::string n){
-        name_=n;
-    }
+// setters
+void Student::setName(std::string n)
+{
+    name_ = n;
+}
 
-//getters
-    void Student::getAllInfo(){
-     std::cout<< "Name : " << name_ <<"\n"
-            <<"Surname : " << surname_<<"\n"
-            <<"Addres : " << addres_<<"\n"
-            <<"Index Number : "<<indexNumber_<<"\n"
-            <<"Pesel : " << pesel_ <<"\n"
-            <<"Gender : " << gender_ <<"\n";
-    }
+// getters
+void Student::getAllInfo()
+{
+    std::cout << "Name : " << name_ << "\n"
+              << "Surname : " << surname_ << "\n"
+              << "Addres : " << addres_ << "\n"
+              << "Index Number : " << indexNumber_ << "\n"
+              << "Pesel : " << personalID_ << "\n"
+              << "Gender : " << gender_ << "\n";
+}
+
+auto Student::schareBySurname( std::vector<Student>& studnetVector, std::string schareStudent){
+
+    auto result = std::find(studnetVector.begin(),studnetVector.end(),schareStudent);
+    (result != studnetVector.end())
+        ? return result : std::cout<<"No studnet"<<schareStudent<< "in database";      
+
+}
--- Student.h
+++ Student.h
@@ -7,36 +7,31 @@
 // PESEL
 // Gender
 
-
 #ifndef STUDENT_H
 #define STUDENT_H
-
+#include <vector>
 #include <string>
-class Student{
-    private:
+#include <algorithm>
+class Student
+{
+private:
     std::string name_;
     std::string surname_;
     std::string addres_;
     int indexNumber_;
-    int pesel_;
+    int personalID_;
     std::string gender_;
 
-    public:
-    //constructors
+public:
+    // constructors
 
-        Student();
-        Student(std::string,std::string,std::string,int,int,std::string);
-    //setters
-        void setName(std::string);
-    //getters
-        void getAllInfo();
+    Student();
+    Student(std::string, std::string, std::string, int, int, std::string);
+    // setters
+    void setName(std::string);
+    // getters
+    void getAllInfo();
+    auto schareBySurname( std::vector<Student>&,std::string);
 };
-
-
-
-
-
-
-
 
 #endif // STUDENT_H
